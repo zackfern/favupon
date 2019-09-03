083bf9918f65606a37ce623c1bf3daee02df99b3
Zack Fernandes <zack@ohnoco.com>
2019-09-02 23:16:28 -0700

    Store just current_user ID instead of entire object

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < ActionController::Base
   # Public: Set the current user in the session.
   def current_user=(user)
-    session[:current_user] = user
+    session[:current_user_id] = user.id
   end
 
   # Public: Get the current user in the session.
-  def current_user(user)
-    session[:current_user]
+  def current_user
+    @current_user ||= User.find(session[:current_user_id])
   end
   helper_method :logged_in?
 
   # Public: Determine if a user is logged in.
   def logged_in?
-    session[:current_user].present?
+    session[:current_user_id].present?
   end
   helper_method :logged_in?
 end
