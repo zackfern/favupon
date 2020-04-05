8f782508bb55d2684152094731644ea03337cbab
Zack Fernandes <zack@ohnoco.com>
2020-04-04 20:09:25 -0700

    Rescue more Twitter errors, show the message

--- app/helpers/favorite_tweets_helper.rb
+++ app/helpers/favorite_tweets_helper.rb
@@ -3,8 +3,8 @@ module FavoriteTweetsHelper
     begin
       oembed = current_user.twitter_client.oembed(tweet.twitter_id)
       oembed.html.html_safe
-    rescue Twitter::Error::NotFound
-      "Ugh, that tweet was deleted. What a bummer."
+    rescue Twitter::Error => e
+      "Oh no, there was an error loading that tweet: #{e.message}"
     end
   end
 end
